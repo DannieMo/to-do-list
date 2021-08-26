ea713cea8451032c1fce06de66f5ae1ee3991900
DannieMo <danielowiri4zoe@gmail.com>
2021-08-26 10:46:43 +0100

    Ádd sort algo

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import './style.css';
 
-const list = [
+let list = [
   {
     completed: false,
     description: 'Make sure that there are no linter errors.',
-    index: 1
+    index: 4
   },
   {
     completed: false,
     description: 'Make sure that you used correct GitHub Flow.',
-    index: 1
+    index: 3
   },
   {
     completed: false,
     description: 'Make sure that you documented your work in a professional way.',
-    index: 1
+    index: 2
   },
   {
     completed: false,
@@ -23,9 +23,10 @@ const list = [
   },
 ]
 
-const page = document.getElementById('todo-items');
+list = list.sort((a,b) =>  a.index - b.index);
 
 const show = () => {
+  const page = document.getElementById('todo-items');
   list.forEach((e) => {
     const li = document.createElement('li');
     li.append(e.description);
