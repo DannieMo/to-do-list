69c07b7fc8d7284eea9b6b9806fe9168e5f45f61
DannieMo <danielowiri4zoe@gmail.com>
2021-08-26 04:13:58 +0100

    Add css styling

--- src/index.js
+++ src/index.js
@@ -24,3 +24,40 @@ function newTask(){
   }
   todo.push(nextTodo)
 }
+
+button.addEventListener("click", () => {
+  //pushing into the array
+  const inputValue = input.value;
+
+  //Printing Out Our Todo
+  if(input.value == ""){
+    
+  }else{
+    todoItems.innerHTML += 
+    `<li>
+      <input type="checkbox">
+      <label for='checkbox' id='checkbox'>${inputValue}</label>
+      <div>
+        <i class="fas fa-ellipsis-v dot-class"></i>
+        <i class="far fa-trash-alt hide trash-can"></i>
+      </div>
+    </li>
+  `
+  }
+  input.value = "";
+})
+
+todoItems.addEventListener('click', (e) => {
+  if (e.target.hasAttribute('type')) {
+    e.target.nextSibling.classList.toggle('strike');
+
+  }
+
+  if(e.target.classList.contains("dot-class")){
+    e.target.parentElement.parentElement.classList.toggle("color");
+    e.target.classList.toggle("hide");
+    e.target.nextSibling.nextSibling.classList.toggle("hide")
+
+    
+  }
+})
