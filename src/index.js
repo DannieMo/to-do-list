89ccae80bb45274f9e22f9f2eaae57cff754a930
DannieMo <danielowiri4zoe@gmail.com>
2021-08-26 10:34:51 +0100

    Ádd show function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import './style.css';
+
+const list = [
+  {
+    completed: false,
+    description: 'Make sure that there are no linter errors.',
+    index: 1
+  },
+  {
+    completed: false,
+    description: 'Make sure that you used correct GitHub Flow.',
+    index: 1
+  },
+  {
+    completed: false,
+    description: 'Make sure that you documented your work in a professional way.',
+    index: 1
+  },
+  {
+    completed: false,
+    description: 'Then we write code',
+    index: 1
+  },
+]
+
+const page = document.getElementById('todo-items');
+
+const show = () => {
+  list.forEach((e) => {
+    const li = document.createElement('li');
+    li.append(e.description);
+    page.append(li);
+  });
+}
+
+show();
