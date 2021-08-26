e7b80f40159c1ca2b428400d81ea1bcda6f64963
DannieMo <danielowiri4zoe@gmail.com>
2021-08-26 03:47:57 +0100

    Add javascript arrays

--- src/index.js
+++ src/index.js
@@ -8,3 +8,19 @@ const checkbox = document.getElementsByTagName('input[type=checkbox]');
 let dot = document.getElementsByClassName("dot-class");
 let trash = document.getElementsByClassName("trash-can")
 let label = document.getElementsByTagName("label");
+
+let todo = [
+  {
+  description: String,
+  completed: Boolean,
+  index: Number
+  }
+];
+function newTask(){
+  let nextTodo ={
+    description: input.value,
+    completed:true,
+    index: 1,
+  }
+  todo.push(nextTodo)
+}
