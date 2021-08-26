370ffd9579b4eccad666bf05cb89109b3e7cdc18
DannieMo <danielowiri4zoe@gmail.com>
2021-08-26 03:46:53 +0100

    Add javascript files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './style.css';
 
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const todoItems = document.getElementById("todo-items");
+let strike = document.getElementsByClassName("strike")
+const checkbox = document.getElementsByTagName('input[type=checkbox]');
+let dot = document.getElementsByClassName("dot-class");
+let trash = document.getElementsByClassName("trash-can")
+let label = document.getElementsByTagName("label");
